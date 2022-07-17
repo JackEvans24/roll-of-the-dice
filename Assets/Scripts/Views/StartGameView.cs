b862d944f4f1c3c933a9234fa7b8e2933090e94a
Jack Evans <jackevans24@googlemail.com>
2022-07-17 18:06:40 +0100

    Add a start of game view with twitter link

---
+++ Assets/Scripts/Views/StartGameView.cs
@@ -0,0 +1,28 @@
+﻿using RollOfTheDice.Controllers;
+using UnityEngine;
+using Zenject;
+
+namespace RollOfTheDice.Views
+{
+    public class StartGameView : MonoBehaviour
+    {
+        private GameController _gameController;
+        
+        [Inject]
+        public void Constructor(GameController gameController)
+        {
+            _gameController = gameController;
+        }
+
+        public void StartGame()
+        {
+            _gameController.Reset();
+            gameObject.SetActive(false);
+        }
+
+        public void OpenTwitter()
+        {
+            Application.OpenURL("https://twitter.com/Jevans_Games");
+        }
+    }
+}
