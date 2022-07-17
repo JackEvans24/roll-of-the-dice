9d8b37dad051334b906275b85b3f7e7b1e2a5bab
Jack Evans <jackevans24@googlemail.com>
2022-07-17 17:10:00 +0100

    Add attack, hurt, shield sounds to player and enemy

---
+++ Assets/Scripts/Models/PlayerUpdate.cs
@@ -0,0 +1,45 @@
+﻿namespace RollOfTheDice.Models
+{
+    public class PlayerUpdate
+    {
+        public UpdateType UpdateType;
+        public Player Player;
+        public string AnimationName;
+
+        private PlayerUpdate(Player player, UpdateType updateType)
+        {
+            UpdateType = updateType;
+            Player = player;
+            AnimationName = updateType.ToString();
+        }
+
+        public static PlayerUpdate Attack(Player player) => new PlayerUpdate(player, UpdateType.Attack);
+        public static PlayerUpdate Shield(Player player) => new PlayerUpdate(player, UpdateType.Shield);
+        public static PlayerUpdate Hurt(Player player) => new PlayerUpdate(player, UpdateType.Hurt);
+    }
+    
+    public class EnemyUpdate
+    {
+        public UpdateType UpdateType;
+        public Enemy Enemy;
+        public string AnimationName;
+
+        private EnemyUpdate(Enemy enemy, UpdateType updateType)
+        {
+            UpdateType = updateType;
+            Enemy = enemy;
+            AnimationName = updateType.ToString();
+        }
+
+        public static EnemyUpdate Attack(Enemy enemy) => new EnemyUpdate(enemy, UpdateType.Attack);
+        public static EnemyUpdate Shield(Enemy enemy) => new EnemyUpdate(enemy, UpdateType.Shield);
+        public static EnemyUpdate Hurt(Enemy enemy) => new EnemyUpdate(enemy, UpdateType.Hurt);
+    }
+
+    public enum UpdateType
+    {
+        Attack,
+        Shield,
+        Hurt
+    }
+}
--- Assets/Scripts/Views/PlayerView.cs
+++ Assets/Scripts/Views/PlayerView.cs
@@ -19,6 +19,12 @@ namespace RollOfTheDice.Views
         [Header("Animator")]
         [SerializeField] private Animator _animator;
 
+        [Header("Audio")]
+        [SerializeField] private AudioSource _audioSource;
+        [SerializeField] private AudioClip _attackClip;
+        [SerializeField] private AudioClip _shieldClip;
+        [SerializeField] private AudioClip _hurtClip;
+
         private GameController _gameController;
         
         [Inject]
@@ -40,10 +46,19 @@ namespace RollOfTheDice.Views
             _shieldLabel.text = player.Shield.ToString();
         }
 
-        private void PlayerUpdate(PlayerUpdateData updateData)
+        private void PlayerUpdate(PlayerUpdate updateData)
         {
             _animator.SetTrigger(updateData.AnimationName);
             UpdateDetails(updateData.Player);
+
+            var clip = updateData.UpdateType switch
+            {
+                UpdateType.Attack => _attackClip,
+                UpdateType.Hurt => _hurtClip,
+                UpdateType.Shield => _shieldClip,
+                _ => throw new NotImplementedException()
+            };
+            _audioSource.PlayOneShot(clip);
         }
 
         private void OnDestroy()
